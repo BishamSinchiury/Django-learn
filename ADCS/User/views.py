478b659365e32951dbf1add3f61c0b12b6224b4a
BishamSinchiury <Bishamsinchiury1116@gmail.com>
2025-06-19 12:23:58 +0545

    dfas

--- ADCS/User/views.py
+++ ADCS/User/views.py
@@ -4,4 +4,9 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
-    return render(request, 'website/index.html')
+    data = [
+        {'id': 1, 'title': 'Task 1'},
+        {'id': 2, 'title': 'Task 2'},
+        {'id': 3, 'title': 'Task 3'},
+    ]
+    return render(request, 'website/index.html', context={"tasks": data})
